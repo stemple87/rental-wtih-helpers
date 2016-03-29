13c714541abd02e41ef040c21bb0f3b713505e85
Matthew Newell <mnewell10@gmail.com>
2016-03-29 11:01:01 -0700

    Destroy a record

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,8 +1,14 @@
+//only route handlers may create, delete or update objects in our data store
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
  actions: {
+   delete(rental) {
+     if(confirm('Are you sure you want to delete this rental forever? ever ever!')) {
+       this.sendAction('destroyRental', rental);
+     }
+   },
     imageShow: function() {
       this.set('isImageShowing', true);
     },
