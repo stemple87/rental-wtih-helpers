beb7b9ddf3209dc4fb690f8c4c9f3bc00bf27c9a
Matthew Newell <mnewell10@gmail.com>
2016-03-29 10:21:42 -0700

    Add a componenet in order to have index.hbs show less inside the {{each}} loop

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,3 +1,5 @@
+//This file is essentially a 'blueprint' listing out the attributes each of our rentals should contain. Let's add the same attributes we previously used in our hard-coded array of JavaScript objects: Owner, city, type, image, and bedrooms:
+
 import DS from 'ember-data';
 
 export default DS.Model.extend({
